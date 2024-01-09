aa4c46429ec5fb64d50d9c638ed59298716210f1
jl663 <jl663@M-00203>
2024-01-09 10:40:20 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace testConsoleHilton2
+{
+    internal class PrintStuff
+    {
+        private string language;
+        public PrintStuff ()
+        {
+            language = "EN";
+        }
+        //constructor
+        public PrintStuff(string temp)
+        {
+            language = temp;
+        }
+
+        public void PrintName(string name)
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello, " + name + "!!!");
+            }
+            if (language == "PT")
+            {
+                System.Console.WriteLine("Oi, " + name + "!!!");
+            }
+            if (language == "DE")
+            {
+                System.Console.WriteLine("Hallo, " + name + "!!!");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using testConsoleHilton2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff('EN') ;
+
+        string name = "";
+
+        System.Console.WriteLine("Please enter your name");
+        name = System.Console.ReadLine();
+        ps.PrintName(name);
+    }
+    // make the method static, use the full class name. 
+    // make the method NOT static, use an instance of the class.
+
+
+}
